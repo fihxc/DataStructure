5199d7228ccaa0459ce24610d8039cc7c4fd291c
DaDaMrX <dadamr_x@163.com>
2016-10-22 15:18:26 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+const int INF = 0x7f7f7f7f;
+const int N = 1e6 + 10;
+
+int main()
+{
+    printf("Hello world!\n");
+    int a;
+    scanf("%d", &a);
+    a += 2;
+    printf("a = %d\n", a);
+    return 0;
+}
