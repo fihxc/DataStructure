b25fe5b7e2cd2c021092426bdd32ae0b5aa6955b
DaDaMrX <dadamr_x@163.com>
2016-11-15 14:13:51 +0800

    11.15

--- .main.cpp
+++ .main.cpp
@@ -5,54 +5,10 @@ using namespace std;
 const int INF = 0x7f7f7f7f;
 const int N = 1e3 + 10;
 
-struct Node
-{
-    int data;
-    Node *left, *right;
-    Node(): left(NULL), right(NULL) {}
-    Node(int data, Node *left, Node *right): 
-        data(data), left(left), right(right) {}
-};
-Node *H;
 
-void init()
-{
-    H = new data;
-}
 
-void insert(char s[])
-{
-    int len = strlen(s);
-    Node *p = H;
-    bool flag = false;
-    for (int i = 0; i < len; i++)
-    {
-        if (s[i] == 0)
-        {
-            if (p->left)
-        }
-        else
-        {
-
-        }
-    }
-}
-
-char s[N];
 int main()
 {
-    init();
-    int n;
-    scanf("%d", &n);
-    while (n--)
-    {
-        scanf("%s", s);
-        if (!insert(s))
-        {
-            printf("%s\n", s);
-            return 0;
-        }
-    }
-    printf("YES\n");
+    
     return 0;
 }
